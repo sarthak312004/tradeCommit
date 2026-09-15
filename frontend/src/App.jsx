57ef81e91a5df7d5781a258e4254195d27689b01
srthk31 <sarthak31122004m@gmail.com>
2026-09-15 21:47:32 +0530

    initialize tradeCommit project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='text-3xl font-bold underline'>TradeCommit</h1>
-    <p>trade journal</p>
+    <h1 className='text-2xl font-bold underline italic '>TradeCommit</h1>
+    <div className='h-50 w-50 bg-white rounded-2xl'>
+      <p className='text-black'>Trade Card</p>
+    </div>
     </>
   )
 }
